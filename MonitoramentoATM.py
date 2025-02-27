5c2315b9817d83801c9344213cc69d5d941407c0
gabrieliwakura <gabriel.iwakura@sptech.school>
2025-02-27 18:52:47 -0300

    Atualizando os selects

--- MonitoramentoATM.py
+++ MonitoramentoATM.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="rootroot",
+  password="12345678",
   database="cpu_maquina2"
 )
 
@@ -22,35 +22,25 @@ continuar = True
 while continuar:
     opcao = input('Selecione uma opção: 1 - CPU | 2 - MEMÓRIA | 3 - DISCO ')
     match opcao:
-        case '1':
-            metrica = input("Qual tipo de medida deseja visualizar: 1 - Byte | 2 - Percentual ")
-            
-            if metrica == '1':
-                mycursor.execute(f"SELECT idATM, cpuByte FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
-                myresult = mycursor.fetchall() 
+        case '1':            
+            mycursor.execute(f"SELECT idATM, DATE_FORMAT(horaRegistro, '%h:%i:%s %d/%m/%y'),cpuPercentual FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
+            myresult = mycursor.fetchall()
 
-                for x in myresult:
-                    print(x)
-
-            elif metrica == '2':
-                mycursor.execute(f"SELECT idATM, cpuPercentual FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
-                myresult = mycursor.fetchall()
-
-                for x in myresult:
-                    print(x)
+            for x in myresult:
+                print(x)
         
         case '2':
             metrica = input("Qual tipo de medida deseja visualizar: 1 - Byte | 2 - Percentual ")
 
             if metrica == '1':
-                mycursor.execute(f"SELECT idATM, ramByte FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
+                mycursor.execute(f"SELECT idATM, DATE_FORMAT(horaRegistro, '%h:%i:%s %d/%m/%y'), ramByte FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
                 myresult = mycursor.fetchall()
 
                 for x in myresult:
                     print(x)
 
             elif metrica == '2':
-                mycursor.execute(f"SELECT idATM, ramPercentual FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
+                mycursor.execute(f"SELECT idATM, DATE_FORMAT(horaRegistro, '%h:%i:%s %d/%m/%y'), ramPercentual FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
                 myresult = mycursor.fetchall()
 
                 for x in myresult:
@@ -60,14 +50,14 @@ while continuar:
             metrica = input("Qual tipo de medida deseja visualizar: 1 - Byte | 2 - Percentual ")
 
             if metrica == '1':
-                mycursor.execute(f"SELECT idATM, discoByte FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
+                mycursor.execute(f"SELECT idATM, DATE_FORMAT(horaRegistro, '%h:%i:%s %d/%m/%y'), discoByte FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
                 myresult = mycursor.fetchall()
 
                 for x in myresult:
                     print(x)
 
             elif metrica == '2':
-                mycursor.execute(f"SELECT idATM, discoPercentual FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
+                mycursor.execute(f"SELECT idATM, DATE_FORMAT(horaRegistro, '%h:%i:%s %d/%m/%y'), discoPercentual FROM atms JOIN metricas on idATM = fkATM WHERE idATM = {maquina}")
                 myresult = mycursor.fetchall()
 
                 for x in myresult:
@@ -80,5 +70,6 @@ while continuar:
 visualizar_outra_maquina = input("Deseja visualizar outra máquina? S/N ").lower()
 if visualizar_outra_maquina == 's':
     maquina = int(input("Digite a máquina que deseja monitorar: "))
+    continuar = True
 else:
     print("Encerrando o programa.")
